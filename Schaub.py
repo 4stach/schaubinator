d3cae0c91c1abd1b7f156a567d403108cfbe0059
4stach <88769848+4stach@users.noreply.github.com>
2023-05-25 19:28:41 +0200

    Add files via upload

--- Schaub.py
+++ Schaub.py
@@ -1,24 +1,62 @@
+
 import random
+from bs4 import BeautifulSoup
+import requests
 
-class Zdania:
-     def __init__(self, stwierdzenie, puenta):
-         self.stwierdzenie = stwierdzenie
-         self.puenta = puenta
+class Schaubinator():
 
-     stwierdzenie = [
-         "Just pushing myself for more rounds at a higher pace in training. Your focus should be grounded in what you do, and that's fighting.",
-         "You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something - your gut, destiny, life, karma, whatever."
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    }
+    strona1 = []
+    url = 'http://www.feelthewords.com/brendan-schaub-quotes'
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result1 = soup.find_all("p", class_="color-dark text-center")
+    for p_tag in result1:
+        nohtml_result = p_tag.text
+        strona1.append(nohtml_result)
+        if nohtml_result is not None:
+            print(type(nohtml_result))
+    if nohtml_result in strona1:
+        print(strona1)
+    else:
+        print("niedziala")
 
-     ]
-     puenta = [
-         "I'm getting my cardio that way.",
-         "This approach has never let me down, and it has made all the difference in my life."
-     ]
-     def schaub_random():
-         zart = random.choice(Zdania.stwierdzenie) + random.choice(Zdania.puenta)
-         return "".join(zart)
+    for x in range(len(strona1)):
+        print(strona1[x])
+
+    ###### Reddit Input
+    headers2 = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    }
+    strona2 = []
+    url2 = 'http://www.reddit.com/r/tfatk/comments.json'
+    page2 = requests.get(url2, headers=headers2)
+    soup2 = BeautifulSoup(page2.content, "html.parser")
+    result2 = soup.find_all("a", class_="md")
+
+    for a_tag in result2:
+        reddit_result = a_tag.text
+        strona2.append(reddit_result)
+        if reddit_result is None:
+            print(type(reddit_result))
+        if reddit_result in strona2:
+            print(strona2)
+        else:
+            print("niedziala")
+    zdania = strona1 + strona2
+
+    def schaub_random():
+        zart = random.choice(strona1)
+        return zart
+
+    if __name__ == "__main__":
+        print(schaub_random())
 
 
 
-print(Zdania.schaub_random())
+
+
+
 
